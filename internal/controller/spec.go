aa5d9aa3851839480f2ef4d49d4ae23971c23cb6
Juan Castillo <castillobgr@gmail.com>
2025-01-19 16:44:25 -0500

    spec: Add ConsumerAcceptList, NatSubnetFQNs & more

--- internal/controller/spec.go
+++ internal/controller/spec.go
@@ -1,10 +1,29 @@
 package controller
 
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/go-logr/logr"
+	"go.uber.org/multierr"
+)
+
 // Spec is the configuration for the controller, which is loaded from an annotation on the
 // StatefulSet.
 type Spec struct {
-	Prefix    string                `json:"prefix"`
-	NodePorts map[string]PortConfig `json:"node_ports"`
+	Prefix             string                `json:"prefix"`
+	IP                 *string               `json:"ip,omitempty"`
+	GlobalAccess       *bool                 `json:"global_access,omitempty"`
+	ConsumerAcceptList []*Consumer           `json:"consumer_accept_list,omitempty"`
+	NatSubnetFQNs      []string              `json:"nat_subnet_fqns,omitempty"`
+	NodePorts          map[string]PortConfig `json:"node_ports"`
+}
+
+// See https://cloud.google.com/compute/docs/reference/rest/v1/serviceAttachments
+type Consumer struct {
+	NetworkFQN      *string `json:"network_fqn,omitempty"`
+	ConnectionLimit uint32  `json:"connection_limit,omitempty"`
+	ProjectIdOrNum  *string `json:"project_id_or_num,omitempty"`
 }
 
 type PortConfig struct {
