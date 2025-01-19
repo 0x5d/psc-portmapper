22af40fbcd6b6caf2210b900e420c45a3e99c252
Juan Castillo <castillobgr@gmail.com>
2025-01-19 16:45:23 -0500

    spec: Add validation func

---
+++ internal/controller/spec_test.go
@@ -0,0 +1,102 @@
+package controller
+
+import (
+	"testing"
+
+	"github.com/go-logr/logr/testr"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateSpec(t *testing.T) {
+	tests := []struct {
+		name        string
+		spec        *Spec
+		expectedErr string
+	}{{
+		name:        "Fails if the spec is nil",
+		expectedErr: "spec is nil",
+	}, {
+		name: "Returns no errors for a spec with only NetworkFQN",
+		spec: &Spec{
+			NatSubnetFQNs: []string{"projects/my-project-123/regions/us-east1/subnetworks/my-subnet"},
+			ConsumerAcceptList: []*Consumer{{
+				NetworkFQN:      stringPtr("projects/my-project-123/global/networks/my-vpc"),
+				ConnectionLimit: 10,
+			}},
+		},
+	}, {
+		name: "Returns no errors for a spec with only ProjectIdOrNum",
+		spec: &Spec{
+			NatSubnetFQNs:      []string{"projects/my-project-123/regions/us-east1/subnetworks/my-subnet"},
+			ConsumerAcceptList: []*Consumer{{ProjectIdOrNum: stringPtr("project1"), ConnectionLimit: 10}},
+		},
+	}, {
+		name: "Fails if NetworkFQN is invalid",
+		spec: &Spec{
+			NatSubnetFQNs:      []string{"projects/my-project-123/regions/us-east1/subnetworks/my-subnet"},
+			ConsumerAcceptList: []*Consumer{{NetworkFQN: stringPtr("net")}},
+		},
+		expectedErr: "invalid value for network_fqn (\"net\") in consumer_list[0], expected format: projects/<project-id>/global/networks/<network-name>",
+	}, {
+		name: "Fails if both NetworkFQN and ProjectIdOrNum are set",
+		spec: &Spec{
+			NatSubnetFQNs: []string{"projects/my-project-123/regions/us-east1/subnetworks/my-subnet"},
+			ConsumerAcceptList: []*Consumer{{
+				NetworkFQN:      stringPtr("projects/my-project-123/global/networks/my-vpc"),
+				ProjectIdOrNum:  stringPtr("project1"),
+				ConnectionLimit: 10,
+			}},
+		},
+		expectedErr: "network_fqn and project_id_or_num can't both be set in consumer_list[0]",
+	}, {
+		name: "Fails if neither NetworkFQN nor ProjectIdOrNum are set",
+		spec: &Spec{
+			NatSubnetFQNs:      []string{"projects/my-project-123/regions/us-east1/subnetworks/my-subnet"},
+			ConsumerAcceptList: []*Consumer{{ConnectionLimit: 10}},
+		},
+		expectedErr: "either network_fqn or project_id_or_num must be set in consumer_list[0]",
+	}, {
+		name: "It's OK if ConnectionLimit is not set",
+		spec: &Spec{
+			NatSubnetFQNs:      []string{"projects/my-project-123/regions/us-east1/subnetworks/my-subnet"},
+			ConsumerAcceptList: []*Consumer{{ProjectIdOrNum: stringPtr("my-project")}},
+		},
+	}, {
+		name:        "Fails if a NatSubnetFQNs is empty",
+		spec:        &Spec{},
+		expectedErr: "nat_subnet_fqns is empty",
+	}, {
+		name: "Fails if a NatSubnetFQN is invalid",
+		spec: &Spec{
+			NatSubnetFQNs: []string{"subnet", "projects/my-project-123/regions/us-east1//my-subnet"},
+		},
+		expectedErr: "invalid value for nat_subnet_fqns[0] (\"subnet\"), expected format: projects/<project-id>/regions/<region-name>/subnetworks/<subnetwork-name>; invalid value for nat_subnet_fqns[1] (\"projects/my-project-123/regions/us-east1//my-subnet\"), expected format: projects/<project-id>/regions/<region-name>/subnetworks/<subnetwork-name>",
+	}, {
+		name: "Accumulates errors",
+		spec: &Spec{
+			NatSubnetFQNs: []string{"projects/my-project-123/regions/us-east1/subnetworks/my-subnet"},
+			ConsumerAcceptList: []*Consumer{
+				{ProjectIdOrNum: stringPtr("my-project"), NetworkFQN: stringPtr("projects/my-project-123/global/networks/my-vpc")},
+				{ConnectionLimit: 0},
+				{NetworkFQN: stringPtr("net")},
+			},
+		},
+		expectedErr: "network_fqn and project_id_or_num can't both be set in consumer_list[0]; either network_fqn or project_id_or_num must be set in consumer_list[1]; invalid value for network_fqn (\"net\") in consumer_list[2], expected format: projects/<project-id>/global/networks/<network-name>",
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			log := testr.New(t)
+			err := validateSpec(log, tt.spec)
+			if tt.expectedErr != "" {
+				require.EqualError(t, err, tt.expectedErr)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
+
+func stringPtr(s string) *string {
+	return &s
+}
